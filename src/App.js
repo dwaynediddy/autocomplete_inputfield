0e74825a8deee5d935cda9a2c4d357860ced0b35
dwaynediddy <burrows.dwayne@icloud.com>
2021-05-24 11:39:43 +0200

    calling api successfuly

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
 import './App.css'
-import React, { useState, useEffect } from 'react'
+import React,{ useState, useEffect } from 'react'
 import axios from 'axios'
 
 
 function App() {
+ const[ name, setName ] = useState([])
 
+ useEffect(() => {
+   //load api data and set it to state
+   const loadNames = async () => {
+   
+   const res = await axios.get('https://jsonplaceholder.typicode.com/users')
+  //check the response by logging in console
+   console.log(res.data)
 
-  return (
+   setName(res.data)
+   }
+   loadNames()
+ }, [])
+
+ 
+ 
+//  //check for matching changes
+//  const onChangeHandler = text => {
+//   //  setName(text)
+//   //  setSuggestions(suggestions)
+
+//    let suggestions = []
+
+//    if(text.length > 0) {
+//      suggestions = name.filter(name => {
+    
+//       const regex = new RegExp(`${text}`, 'gi')
+
+//       return name.name.suggestions(regex)
+//      })
+//    }
+//   //  setSuggestions(suggestions)
+//   //  setText(text)
+//  }
+ return (
     <div className="App">
       <form>
         <label>
-          First Name:
-          <input type="text" name="first_name" />
-        </label>
-        <label>
-          Last Name:
-          <input type="text" name="last_name" />
+          Name:
+          <input 
+            type="text" 
+            name="first_name" 
+            // onChangeHandler={e => onChangeHandler(e.target.name)} 
+            />
         </label>
         <input type="submit" value="Submit" />
       </form>
