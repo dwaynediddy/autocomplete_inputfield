de5f7a558bc3d92e60789f911e5a7f1696e396f1
dwaynediddy <burrows.dwayne@icloud.com>
2021-05-24 10:28:55 +0200

    form built axios added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>
+          First Name:
+          <input type="text" name="first_name" />
+        </label>
+        <label>
+          Last Name:
+          <input type="text" name="last_name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
